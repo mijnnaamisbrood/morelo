abbb6ed009f57fa38cee65ef4e7d2b05e4767e4f
Morelo <tellurium.coin@gmail.com>
2020-03-11 16:31:33 +0200

    DNSoff

--- src/common/updates.cpp
+++ src/common/updates.cpp
@@ -47,8 +47,7 @@ namespace tools
 
     // All four ArQ-Net domains have DNSSEC on and valid
     static const std::vector<std::string> dns_urls = {
-      "updates.morelo-project.rf.gd",
-      "morelo-project.go.ro/updates"
+
     };
 
     if (!tools::dns_utils::load_txt_records_from_dns(records, dns_urls))
